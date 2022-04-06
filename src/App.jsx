ee1237ca974085f18211e5aa44e289dbb161bfab
jcena <you@example.com>
2022-04-06 11:40:52 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <>
+    <h1>Welcome to React </h1>
+    <p>Has to get more</p>
+    </>
+  );
+};
+
+export default () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+);
